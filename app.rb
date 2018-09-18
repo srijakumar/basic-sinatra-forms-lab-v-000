02dd4a0d019e5b2f3345c599f7809097a0b01f10
Srija S <srijakumar7@gmail.com>
2018-09-18 15:55:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ class App < Sinatra::Base
 
   post '/team' do
     @name = params[:name]
-  @coach = params[:coach]
-  @pg = params[:pg]
-  @sg = params[:sg]
-  @pf = params[:pf]
-  @sf = params[:sf]
-  @c = params[:c]
-  erb :team
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
   end
 
 end
